627068b3d4cdc9dc0a2eca748b5e1aeac50734be
masondoc <128937741+masondoc@users.noreply.github.com>
2023-05-29 13:32:57 -0500

    Update main.ino

--- waveArms/main.ino
+++ waveArms/main.ino
@@ -22,31 +22,57 @@ void setup() {
   myservo3.attach(11);  // attaches the servo on pin 11 to the servo object
 }
 
-void waveArm(Servo servo, int start, int end) {
-  // wave the arm from start to end degrees
-  for (int pos = start; pos <= end; pos += 1) {
-    servo.write(pos);
-    delay(5);
-  }
-
-  delay(3000);
-
-  // wave the arm from end to start degrees
-  for (int pos = end; pos >= start; pos -= 1) {
-    servo.write(pos);
-    delay(5);
-  }
-
-  delay(3000);
-}
-
 void loop() {
   // reset arms
   myservo1.write(180);
   myservo2.write(180);
   myservo3.write(180);
 
-  waveArm(myservo1, 0, 180);
-  waveArm(myservo2, 0, 180);
-  waveArm(myservo3, 0, 180);
+  // wave the arm 1 from 0 to 180 degrees
+  for (pos1 = 0; pos1 <= 180; pos1 += 1) {
+    myservo1.write(pos1);
+    delay(5);
+  }
+
+  delay(10000);
+
+  // wave the arm 1 from 180 to 0 degrees
+  for (pos1 = 180; pos1 >= 0; pos1 -= 1) {
+    myservo1.write(pos1);
+    delay(5);
+  }
+
+  delay(3000);
+
+  // wave the arm 2 from 0 to 180 degrees
+  for (pos2 = 0; pos2 <= 180; pos2 += 1) {
+    myservo2.write(pos2);
+    delay(5);
+  }
+
+  delay(10000);
+
+  // wave the arm 2 from 180 to 0 degrees
+  for (pos2 = 180; pos2 >= 0; pos2 -= 1) {
+    myservo2.write(pos2);
+    delay(5);
+  }
+
+  delay(3000);
+
+  // wave the arm 3 from 0 to 180 degrees
+  for (pos3 = 0; pos3 <= 180; pos3 += 1) {
+    myservo3.write(pos3);
+    delay(5);
+  }
+
+  delay(10000);
+
+  // wave the arm 3 from 180 to 0 degrees
+  for (pos3 = 180; pos3 >= 0; pos3 -= 1) {
+    myservo3.write(pos3);
+    delay(5);
+  }
+
+  delay(3000);
 }
